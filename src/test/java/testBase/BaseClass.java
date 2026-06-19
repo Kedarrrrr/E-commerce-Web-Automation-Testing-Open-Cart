f1f595fc8aaa014e39d6771d953f58f0e213d860
Kedarrrrr <132215546+Kedarrrrr@users.noreply.github.com>
2026-06-19 20:13:08 +0530

    Docker Integration

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,13 +1,20 @@
 package testBase;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -16,30 +23,51 @@ public class BaseClass {
 
     protected WebDriver driver;
     public Logger logger;
+    public Properties p;
 
-    @BeforeClass
+    @SuppressWarnings("deprecation")
+	@BeforeClass
     @Parameters({"os", "browser"})
-    public void setup(String os, String br) {
+    public void setup(String os, String br) throws IOException {
 
-    	switch(br.toLowerCase()) {
+    	FileReader fi=new FileReader("/Web_Testing/src/test/resources/Config.properties");
+    	p=new Properties();
+    	p.load(fi);
+    	logger=LogManager.getLogger(this.getClass() );
     	
-    	case "chrome" : driver =new ChromeDriver();break;
-    	case "edge" : driver= new EdgeDriver(); break;
-    	case "firefox" : driver=new FirefoxDriver(); break;
-    	default : System.out.println("Invalid browser name..");return;
-    	
+    	if(p.getProperty("environment").equalsIgnoreCase("remote")) {
+    		DesiredCapabilities cap= new DesiredCapabilities();
+    		
+    		if(os.equalsIgnoreCase("windows")) {
+    			cap.setPlatform(Platform.WIN11);
+    		}else if(os.equalsIgnoreCase("linux")) {
+    			cap.setPlatform(Platform.LINUX);
+    		}else if(os.equalsIgnoreCase("mac")) {
+    			cap.setPlatform(Platform.MAC);
+    		}else {
+    			System.out.println("NO such Operating System exists.");
+    		}
+    		switch(br.toLowerCase()) {
+        	case "chrome": cap.setBrowserName("chrome");break;
+        	case "edge": cap.setBrowserName("MicrosoftEdge");break;
+        	case "firefox": cap.setBrowserName("firefox");break;
+        	}
+    		driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+    		
     	}
+    	if(p.getProperty("environment").equalsIgnoreCase("local")) {
+    		switch(br.toLowerCase()) {
+        	case "chrome":driver=new ChromeDriver();
+        	case "edge": driver=new EdgeDriver();
+        	case "firefox": driver=new FirefoxDriver();
+        	}
+    	}
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get(p.getProperty("URL")); // reading url from properties file.
+		driver.manage().window().maximize();
     	
-        logger = LogManager.getLogger(this.getClass());
-
-        //driver = new ChromeDriver();
-
-        driver.manage().timeouts()
-              .implicitlyWait(Duration.ofSeconds(5));
-
-        driver.manage().window().maximize();
-
-        driver.get("http://localhost/Open%20Cart/");
     }
 
     @AfterClass
